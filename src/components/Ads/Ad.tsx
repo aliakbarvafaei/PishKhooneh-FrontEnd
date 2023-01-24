36c61853f027fdc9e1553affa286175062782751
aliakbarvafaei <72415266+aliakbarvafaei@users.noreply.github.com>
2023-01-24 14:00:40 +0330

    Add graph equal None in Ad

--- src/components/Ads/Ad.tsx
+++ src/components/Ads/Ad.tsx
@@ -383,7 +383,7 @@ const Ad:React.FC<{ad:ads}> = ({ ad })=> {
         </div>
         <div className="flex lg:flex-col lgmin:flex-row lgmin:justify-center items-center lg:justify-center w-full my-[30px] gap-[30px]">
           <Map x={ad.location_y} y={ad.location_x}/>
-          {ad.graph==="" || ad.graph==="Graph" || ad.graph===null ? <></>:<div className="lgmin:w-[60%] lg:w-[90%] h-[400px]"><Chart1 data={JSON.parse(ad.graph.replace(/'/g, '"'))['content'] as any[]} /></div>}
+          {ad.graph==="" || ad.graph==="Graph" || ad.graph==="None" || ad.graph===null ? <></>:<div className="lgmin:w-[60%] lg:w-[90%] h-[400px]"><Chart1 data={JSON.parse(ad.graph.replace(/'/g, '"'))['content'] as any[]} /></div>}
         </div>
       </div>
     </div>
